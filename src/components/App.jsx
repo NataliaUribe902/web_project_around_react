ea25db10668ebf669d05a6cf72eb9c3697a62e67
Natalia <uribenatalia48@gmail.com>
2026-03-21 00:11:38 -0500

    renaming more files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,10 @@
-import Header from "./components/Header/Header";
-import Main from "./components/Main/Main";
-import Footer from "./components/Footer/Footer";
-import { CurrentUserContext } from "./contexts/CurrentUserContext";
+import Header from "./Header/Header";
+import Main from "./Main/Main";
+import Footer from "./Footer/Footer";
+import { CurrentUserContext } from "../contexts/CurrentUserContext";
 import { useState, useEffect } from "react";
-import api from "./utils/api";
-import ImagePopup from "./components/Main/components/ImagePopup/ImagePopup";
+import api from "../utils/api";
+import ImagePopup from "./Main/components/ImagePopup/ImagePopup";
 function App() {
   const [currentUser, setCurrentUser] = useState(null);
   const [popup, setPopup] = useState(null);
